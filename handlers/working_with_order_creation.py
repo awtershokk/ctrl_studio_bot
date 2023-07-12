05bd7f7c51fd60dfc96dbfdb7fa3259cab6ba82f
Grisha Kuznetsov <awtershokk@gmail.com>
2023-07-12 18:14:46 +0700

    по кайфу сделал

--- handlers/working_with_order_creation.py
+++ handlers/working_with_order_creation.py
@@ -6,14 +6,14 @@ conn = create_connection()
 
 def working_with_order_creation(call, bot):
     global conn
-    client_id = call.message.from_user.id
-    client_name = call.message.from_user.first_name
+    client_username = call.from_user.username
+    client_name = call.from_user.first_name
     order_category = info_product['Категория работы']
     order_type = info_product['Тип работы']
     order_deadline = info_product['Сроки']
     order_price = info_product['Расчетная стоимость']
     order_status = info_product['Статус']
-    insert_order(conn, client_id, client_name, order_category, order_type, order_deadline, order_price, order_status)
+    insert_order(conn, client_username, client_name, order_category, order_type, order_deadline, order_price, order_status)
 
 
 

22ba9d371e6a32517ef2f2df9924d1025c659b2e
Grisha Kuznetsov <awtershokk@gmail.com>
2023-07-12 16:53:14 +0700

    исправил мелкие ошибки

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from handlers.start_handler import start_handler
 from handlers.help_handler import help_handler
 from keyboards.main_reply_keyboard import create_reply_keyboard
 from keyboards.order_inline_keyboards import select_order_category, select_order_type, select_order_deadline
-from handlers.сreate_order import choise_order_category,  choise_order_type, choise_order_deadline, last_craete_order_message
+from handlers.сreate_order import choise_order_category,  choise_order_type, choise_order_deadline, last_create_order_message
 
 load_dotenv()
 token = os.getenv('BOT_TOKEN')
@@ -37,6 +37,6 @@ def handle_select_deadline(call):
 @bot.callback_query_handler(func=lambda call: call.data in ['min1week','odna_dve_week','two_week_month','month-twomonth','two-month_more'])
 def handle_create_totalmessage(call):
     choise_order_deadline(call, bot)
-    last_craete_order_message(call, bot)
+    last_create_order_message(call, bot)
 
 bot.polling()

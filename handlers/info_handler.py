44f035749817e83020a6a635a528828262fdd94f
Grisha Kuznetsov <awtershokk@gmail.com>
2023-07-13 14:44:21 +0700

    мелкие изменения

--- handlers/info_handler.py
+++ handlers/info_handler.py
@@ -1,3 +1,4 @@
 import telebot
 def help_handler(message, bot):
-    bot.send_message(message.chat.id, "Помощь", parse_mode="Markdown")
+    infomsg = 'Со всеми правилами и условиями для создания заказа вы можете ознакомиться [здесь](https://telegra.ph/Pravila-i-usloviya-zakaza-07-13).'
+    bot.send_message(message.chat.id, infomsg, parse_mode="Markdown", disable_web_page_preview = True)

abd86d5c046c59cebe7bbd32acaa2f4e6c5fff71
mirout <maksimka.prokopets@mail.ru>
2023-07-14 13:52:45 +0700

    Мои заказы черновик Хауди Хо Хауди Хо Хауди ХоХауди Хо Хауди Хо Хауди Хо Хауди Хо Хауди Хо Хауди Хо Хауди Хо Хауди Хо Хауди Хо Хауди Хо Хауди Хо Хауди Хо Хауди Хо Хауди Хо

--- handlers/clients_order.py
+++ handlers/clients_order.py
@@ -18,3 +18,19 @@ def clients_order(call, bot):
         bot.send_message(chat_id=call.message.chat.id, text=total_message, parse_mode="Markdown")
 
 # client_username, client_id, client_name, order_category, order_type, order_deadline, order_price, order_status):
+def clients_draft_order(call, bot):
+    global conn
+
+    client_id = call.message.from_user.id
+    rows = selecct_client_draft_orders(conn, client_id)
+    for row in rows:
+        ttl1 = '*Ваш заказ:*\n'
+        ttl2 = '*№ заказа* - #1\n'
+        ttl3 = f"*Категория работы* - {row[4]}\n"
+        ttl4 = f"*Тип работы* - {row[5]}\n"
+        ttl5 = f"*Сроки* - {row[6]}\n"
+        ttl6 = f"*Расчетная стоимость* - {row[7]}\n"
+        ttl7 = f'Начало работы: {row[9]}\n'
+        ttl8 = f'Статус: {row[8]}\n'
+        total_message = ttl1 + ttl2 + ttl3 + ttl4 + ttl5 + ttl6 + ttl7 + ttl8
+        bot.send_message(chat_id=call.message.chat.id, text=total_message, parse_mode="Markdown")

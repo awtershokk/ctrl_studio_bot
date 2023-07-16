00c91c4469099ff914dfa3ac25649dab0773b256
mirout <maksimka.prokopets@mail.ru>
2023-07-16 20:07:20 +0700

    Добавление кнопок в черновики Хауди Хо Хауди Хо Хауди ХоХауди Хо Хауди Хо Хауди Хо Хауди Хо Хауди Хо Хауди Хо Хауди Хо Хауди Хо Хауди Хо Хауди Хо Хауди Хо Хауди Хо Хауди Хо

--- handlers/clients_order.py
+++ handlers/clients_order.py
@@ -1,4 +1,6 @@
+from telebot import types
 import telebot
+
 from database.db_functions import selecct_client_draft_orders, select_client_orders ,create_connection
 conn = create_connection()
 status_on_ru ={
@@ -21,11 +23,17 @@ def clients_order(call, bot):
         total_message = ttl1 + ttl2 + ttl3 + ttl4 + ttl5 + ttl6 + ttl7 + ttl8
         bot.send_message(chat_id=call.message.chat.id, text=total_message, parse_mode="Markdown")
 
-# client_username, client_id, client_name, order_category, order_type, order_deadline, order_price, order_status):
+
 def clients_draft_order(call, bot):
     global conn
     client_id = call.from_user.id
     rows = selecct_client_draft_orders(conn, client_id)
+    draft_inline_message = types.InlineKeyboardMarkup()
+    confirm_to_order_button = types.InlineKeyboardButton('✅ Создать заказ', callback_data='confirm_order')
+    delete_draft_button = types.InlineKeyboardButton('⛔️ Удалить', callback_data='delete_draft')
+
+    draft_inline_message.add(confirm_to_order_button)
+    draft_inline_message.add(delete_draft_button)
     for row in rows:
         ttl1 = '*Ваш заказ:*\n'
         ttl2 = '*№ заказа* - #1\n'
@@ -34,5 +42,6 @@ def clients_draft_order(call, bot):
         ttl5 = f"*Сроки* - {row[6]}\n"
         ttl6 = f"*Расчетная стоимость* - {row[7]}\n"
         ttl7 = f'Начало работы: {row[9]}\n'
-        total_message = ttl1 + ttl2 + ttl3 + ttl4 + ttl5 + ttl6 + ttl7 + ttl8
-        bot.send_message(chat_id=call.message.chat.id, text=total_message, parse_mode="Markdown")
+        total_message = ttl1 + ttl2 + ttl3 + ttl4 + ttl5 + ttl6 + ttl7
+
+        bot.send_message(chat_id=call.message.chat.id, text=total_message, parse_mode="Markdown", reply_markup=draft_inline_message)
